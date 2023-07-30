1995cc1251b8fd76e5ba908d7efff401c38be9e0
Faraji Ombonya <farajiombonya@gmail.com>
2023-07-30 11:23:33 +0300

    checking response status

---
+++ Guides/JavaScript Dynamic client side scripting/Asynchronous JavaScript/Introducing asynchronous JavaScript/how to use promises/example04.js
@@ -0,0 +1,13 @@
+// checking response status
+const fetchPromise = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",)
+
+fetchPromise
+    .then((response) => {
+        if(!response.ok) {
+            throw new Error(`HTTTP error: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data[0].name);
+    });
